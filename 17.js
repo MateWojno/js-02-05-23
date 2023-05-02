a5607c5bba7a336b4ebc53584274f71bc7fd377f
MateWojno <mateusz.k.wojno@gmail.com>
2023-05-02 12:05:12 +0200

    W3 schools js-2/5/23

---
+++ 17.js
@@ -0,0 +1,22 @@
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+
+// console.log(z);
+// window.alert(z);
+// document.getElementById("demo").innerHTML = z;
+
+// let gej = 1;
+// let lesba = 2;
+// let lgbt = gej + lesba;
+
+// console.log(lgbt);
+// window.alert(lgbt);
+// document.getElementById("demo").innerHTML = lgbt;
+
+x = 1;
+y = 3;
+z = x + y;
+console.log(z);
+window.alert(z);
+document.getElementById("demo").innerHTML = z;
