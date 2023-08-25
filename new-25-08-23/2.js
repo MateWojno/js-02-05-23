46b94a09ebd1aa456ba7e4799eeedb035c348f93
MateWojno <mateusz.k.wojno@gmail.com>
2023-08-25 13:27:51 +0200

    Events DOM manipulation exc

---
+++ new-25-08-23/2.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello Worldddddd!");
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+    alert("Hello Worlds!");
+});
