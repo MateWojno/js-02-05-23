06cfacb8f0134600d5b778df613809a3e8d15011
MateWojno <mateusz.k.wojno@gmail.com>
2023-09-01 11:37:18 +0200

    New beginning 01-09-23

--- new-25-08-23/2.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('#btn');
-btn.onclick = () => alert("Hello Worldddddd!");
-
-const btn2 = document.querySelector('#btn2');
-btn2.addEventListener('click', () => {
-    alert("Hello Worlds!");
-});
