5abe630b16e2917929e0641f5bce253e1b35bbfe
MateWojno <mateusz.k.wojno@gmail.com>
2023-09-03 20:15:00 +0200

    Obj constructors part2

--- 1.js
+++ 1.js
@@ -1,18 +1,38 @@
-function createPerson(name) {
-    const obj = {};
-    obj.name = name;
-    obj.age = 21;
-    obj.introduceSelf = function() {
-        console.log(`Hi I'm ${this.name}, and I am ${this.age} years old.`);
+// function createPerson(name) {
+//     const obj = {};
+//     obj.name = name;
+//     obj.age = 21;
+//     obj.introduceSelf = function() {
+//         console.log(`Hi I'm ${this.name}, and I am ${this.age} years old.`);
+//     };
+//     return obj;
+// }
+
+// const salva = createPerson("Salva");
+// salva.introduceSelf();
+
+// const frankie = createPerson("Frankie");
+// frankie.introduceSelf();
+
+// const agnes = createPerson("Agnes");
+// agnes.introduceSelf();
+
+//==============//
+function Person(name) {
+    this.name = name;
+    this.introduceSelf = function () {
+        console.log(`Hi I'm ${this.name}.`);
     };
-    return obj;
 }
 
-const salva = createPerson("Salva");
+const salva = new Person("Salva");
+salva.name;
 salva.introduceSelf();
 
-const frankie = createPerson("Frankie");
+const frankie = new Person("Frankie");
+frankie.name;
 frankie.introduceSelf();
 
-const agnes = createPerson("Agnes");
-agnes.introduceSelf();
+
+//===============//
+// const myNofitication = new Notification("Hello!");
